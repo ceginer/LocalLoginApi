0d6221ea52be2092ed0801e3621f522b6d41f9dd
Son <rooneei@naver.com>
2023-05-25 21:08:24 +0900

    [fix]

--- src/main/java/com/example/loginapi/jwt/exception/jwtAccessDeniedHandler.java
+++ src/main/java/com/example/loginapi/jwt/exception/jwtAccessDeniedHandler.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class jwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        log.error("권한없는 사용자의 접근");
+//        log.error("권한없는 사용자의 접근");
         response.sendRedirect("/signup_auth");
     }
 }
